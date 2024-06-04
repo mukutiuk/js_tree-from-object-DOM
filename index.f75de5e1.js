b037f07da31d9ea1c5125d3e93d1998ac15dd2aa
Roma <roma150269@gmail.com>
2024-06-04 22:59:42 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,32 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ul = document.createElement("ul");
+    Object.entries(data).forEach(([key, value])=>{
+        const li = document.createElement("li");
+        li.textContent = key;
+        ul.appendChild(li);
+        if (Object.keys(value).length > 0) createTree(li, value);
+        if (element) element.appendChild(ul);
+        else document.body.appendChild(ul);
+    });
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
